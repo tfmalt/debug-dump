c68b8e931a7be241a7268b458567eb329cef6524
Thomas Malt <thomas@malt.no>
2013-02-08 00:49:23 +0100

    finished implementing inspect function

--- src/debugdump.js
+++ src/debugdump.js
@@ -1,78 +1,84 @@
+/*jslint node: true, vars: true, newcap: true, nomen: true */
+/*global _ */
 /**
  * A simple debug library since phonegap is lacking som dump obj stuff.
  */
-var u_;
-if (typeof exports === 'undefined') {
-    // Running in browser methinks
-    u_ = _;
-} else {
-    u_ = require('underscore');
-}
 
 (function (exports) {
     "use strict";
-    exports.inspect = function (obj, depth, curr) {
-        var depth = depth || 2; // init depth if not set.
-        var curr  = curr  || 0; // Curr is optional so init if not set.
+    var isNode,
+        root,
+        dd,
+        u,
+        _inspect,
+        _inspectArray,
+        _inspectFunction,
+        _inspectObject;
 
-        if (u_.isArray(obj)) {
-            return _inspectArray(obj, depth, curr);
-        }
-        if (u_.isFunction(obj)) {
-            return _inspectFunction(obj, depth, curr);
-        }
-        if (u_.isObject(obj)) {
-            return _inspectObject(obj, depth, curr);
-        }
-        return obj.toString()
+    isNode = (typeof module !== 'undefined' && module.exports) ? true : false;
+    root   = this;
+    dd     = {};
+    u      = (isNode) ? require('underscore') : _;
+
+    _inspect = function (obj, depth, curr) {
+        depth = depth || 2; // init depth if not set.
+
+        if (u.isString(obj)) return "'" + obj + "'";
+        // if (curr == depth+1) return obj.toString();
+
+        curr = curr || 0 ; // Curr is optional so init if not set.
+
+        if (u.isArray(obj))    return _inspectArray(    obj, depth, curr + 1 );
+        if (u.isFunction(obj)) return _inspectFunction( obj, depth, curr + 1 );
+        if (u.isObject(obj))   return _inspectObject(   obj, depth, curr + 1 );
+
+        return obj.toString();
     };
 
-    var _inspectArray = function (obj, depth, curr) {
-        // throw exception if not an array
-        var depth = depth || 2;
-        var curr  = curr  || 0;
 
+    _inspectArray = function (obj, depth, curr) {
+        // throw exception if not an array
+        // console.log("DEBUG: " + curr + ":" + obj.toString());
+        if (depth == curr - 1) return '[object Array]';
         var str = "[ ";
-        u_.each(obj, function (value) {
-            if (u_.isObject(value)) {
-                str += exports.inspect(value, depth, curr+1);
-            } else {
-                str += (u_.isString(value)) ? "'" + value + "', " : value + ", ";
-            }
+        u.each(obj, function (val) {
+            str += _inspect(val, depth, curr) + ", ";
         });
         str = str.replace(/, $/, "");
         str += " ]";
+
         return str;
     };
 
-    var _inspectObject = function (obj, depth, curr) {
-        var depth  = depth || 2;
-        var curr   = curr  || 0;
-        var indent = Array(curr).join("  ");
+    _inspectObject = function (obj, depth, curr) {
+        if (depth == curr - 1) return obj.toString();
 
-        var str = "{\n";
-        u_.each(obj, function(val, key) {
-            str += key + ": ";
-            if (u_.isObject(val)) {
-                str += exports.inspect(val, depth, curr+1);
-            } else {
-                str += (u_.isString(val)) ? "'" + val + "'" : val;
-                str += ", ";
+        var indent = Array(Number(curr)).join("  ");
+        var str    = "{\n";
+        u.each(obj, function (val, key) {
+            if (key.match(/ /g)) {
+                key = "'" + key + "'";
             }
+            str += indent + "  " + key + ": ";
+            str += _inspect(val, depth, curr);
+            str += ",\n";
         });
-        str = str.replace(/, $/, "");
-        str += " }";
+        str = str.replace(/,\n$/m, "");
+        str += "\n" + indent + "}";
 
         return str;
     };
 
-    var _inspectFunction = function (obj, depth, curr) {
-        var depth = depth || 2;
-        var curr  = curr  || 0;
-
+    _inspectFunction = function (obj, depth, curr) {
         // Maybe should throw error if not function 
         return "[Function]";
-    }
+    };
 
-})(typeof exports === 'undefined' ? this['debug'] = {} : exports);
+    if (isNode === true) {
+        module.exports.inspect = _inspect;
+    } else {
+        root.dd = {};
+        root.dd.inspect = _inspect;
+    }
+})();
 
