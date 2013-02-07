2be9c4661368950b382484b9221d9e03b6a23aa2
Thomas Malt <thomas@malt.no>
2013-02-08 00:50:03 +0100

    unit-tests. all tests pass

--- test/debugdumpTest.js
+++ test/debugdumpTest.js
@@ -25,25 +25,27 @@ describe( 'Testing debug', function () {
 
     describe('#inspect() -> _inspectObject()', function () {
         it('Should dump a simple object real nice', function () {
-            var tests = {
-                "{ 4: 4, en: 1, to: 'to', tre: 'tretretre' }": {
-                    en: 1,
-                    to: "to",
-                    "tre": "tretretre",
-                    4: 4
-                },
-                "{ 1: 'en', 2: 'to', 3: 'tre' }": {
-                    3: "tre",
-                    1: "en",
-                    2: "to"
-                }
-            };
-            u.each(tests, function(obj, str) {
-                debug.inspect(obj).should.be.string(str);
-            });
+            debug.inspect({
+                en: 1,
+                to: "to",
+                "tre": "tretretre",
+                4: 4
+            }).should.be.string(
+                "{\n" +
+                "  4: 4,\n" +
+                "  en: 1,\n" +
+                "  to: 'to',\n" +
+                "  tre: 'tretretre'\n" +
+                "}"
+            );
+            debug.inspect({
+                3: "tre",
+                1: "en",
+                2: "to"
+            }).should.be.string("{\n  1: 'en',\n  2: 'to',\n  3: 'tre'\n}"); 
         });
-        /* it('', function () {}); */
     });
+
     describe('#inspect() -> _inspectFunction()', function () {
         it('Should dump a function as it should', function () {
             var test = function () {
@@ -55,13 +57,14 @@ describe( 'Testing debug', function () {
     });
     describe('#inspect() -> complex objects', function () {
         it('Should dump a nested structure in the desired fashion', function () {
-            debug.inspect({
+            var test = {
                 en: "en",
                 2: "to",
                 tre: {
                     en: 1, to: "to"
                 }
-            }).should.be.string(
+            };
+            var result = "" + 
                 "{\n" +
                 "  2: 'to',\n" +
                 "  en: 'en',\n" +
@@ -69,8 +72,82 @@ describe( 'Testing debug', function () {
                 "    en: 1,\n" +
                 "    to: 'to'\n" +
                 "  }\n" +
-                " }"   
-            );
+                "}";
+            // console.log("DEBUG: \n" + debug.inspect(test) + "\n" + result);
+            debug.inspect(test).should.be.string(result);
+        });
+        it('Should dump another structure with arrays hand hashes', function () {
+            var test = {
+                en: [ 1, 2, 3, 'dette er fire', 'fem' ],
+                to: {
+                    'inner en': 1,
+                    'inner to': 2
+                },
+                tre: 'tre'
+            };
+            var result = "" +
+                "{\n" +
+                "  en: [ 1, 2, 3, 'dette er fire', 'fem' ],\n" +
+                "  to: {\n" +
+                "    'inner en': 1,\n" +
+                "    'inner to': 2\n" +
+                "  },\n" +
+                "  tre: 'tre'\n" +
+                "}";
+
+            // console.log("DEBUG: \n" + debug.inspect(test) + "\n" + result);
+            debug.inspect(test).should.be.string(result);
+        });
+        it('Should output an object inside an array perfectly', function () {
+            var test = {
+                "dette er en": "her er en",
+                "jackson 5": ["Jackie", "Tito", { 
+                    "Jermaine": [1964, 1975], 
+                    "Randy": [1975, 1990]
+                }, "Marlon", "Michael"], 
+                "dette er tre": 3
+            };
+            var result = "" + 
+                "{\n" + 
+                "  'dette er en': 'her er en',\n" +
+                "  'jackson 5': [ 'Jackie', 'Tito', [object Object], " +
+                "'Marlon', 'Michael' ],\n" +
+                "  'dette er tre': 3\n" +
+                "}";
+            // console.log("DEBUG: \n" + debug.inspect(test) + "\n" + result);
+            debug.inspect(test).should.be.string(result);        
+        });
+
+        it('Should output the object type of Array correctly', function () {
+            var test = [1, [2, [3, [4, "end"]]]];
+            var result = "[ 1, [ 2, [object Array] ] ]";
+            // console.log("DEBUG: \n" + debug.inspect(test) + "\n" + result);
+            debug.inspect(test).should.be.string(result);
         });
     });
+
+    describe('#inspect() > Must be respectful of depth', function () {
+        var test = { one: { two: { three: { four: { five: { six: 6 }}}}}};
+        it('Should respect default depth of 2', function () {
+            var result = "{\n  one: {\n    two: [object Object]\n  }\n}";
+            debug.inspect(test).should.be.string(result);
+        });
+        it('Should respect depth of 5', function () {
+            var result = "" +
+                "{\n" + 
+                "  one: {\n" +
+                "    two: {\n" +
+                "      three: {\n" +
+                "        four: {\n" +
+                "          five: [object Object]\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+            // console.log("DEBUG: \n" + debug.inspect(test, 5) + "\n" + result);
+            debug.inspect(test, 5).should.be.string(result);
+        }); 
+    });
+
 });
